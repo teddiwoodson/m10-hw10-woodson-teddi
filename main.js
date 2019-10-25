3b334be0a05a80bf815cdf5b8ed3decd140dc757
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-25 10:17:07 -0400

    Edit functions in main.js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 // It is always helpful to use comments in your code!
 
 const boilSpan = document.getElementById('boiling-temp');
-var boilTemp = document.createTextNode("211");
-boilSpan.appendChild(boilTemp);
+
+function boilFunction () {
+  var boilTemp = document.createTextNode("211");
+  boilSpan.appendChild(boilTemp)
+};
+
+boilFunction();
 
 const freezeSpan = document.getElementById('freezing-temp');
-var freezeTemp = document.createTextNode("32");
-freezeSpan.appendChild(freezeTemp);
+function freezeFunction () {
+  var freezeTemp = document.createTextNode("32");
+  freezeSpan.appendChild(freezeTemp);
+};
+
+freezeFunction();
 
 const waterSpan = document.getElementById('water-temp');
-var waterTemp = document.createTextNode("67");
-waterSpan.appendChild(waterTemp);
+function waterFunction () {
+  var waterTemp = document.createTextNode("67");
+  waterSpan.appendChild(waterTemp);
+};
+
+waterFunction();
 
 const temp = document.getElementById('good-temp');
 setTimeout(() => {
