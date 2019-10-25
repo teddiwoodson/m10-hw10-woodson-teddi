8733af9e2848d05228d3497d8bf81101b3283662
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-25 09:52:19 -0400

    Add water tempuratures to html

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // It is always helpful to use comments in your code!
+
+const boilSpan = document.getElementById('boiling-temp');
+var boilTemp = document.createTextNode("211");
+boilSpan.appendChild(boilTemp);
+
+const freezeSpan = document.getElementById('freezing-temp');
+var freezeTemp = document.createTextNode("32");
+freezeSpan.appendChild(freezeTemp);
+
+const waterSpan = document.getElementById('water-temp');
+var waterTemp = document.createTextNode("67");
+waterSpan.appendChild(waterTemp);
